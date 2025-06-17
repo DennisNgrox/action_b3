194295e7e0db8a4ec569625d3a660fa5f8c2acf0
DennisNgrox <dennis.silva@ageri.com.br>
2025-06-17 16:02:55 -0300

    teste

--- scripts/template_alter.py
+++ scripts/template_alter.py
@@ -6,40 +6,41 @@ ZABBIX_PASSWORD = os.getenv('ZABBIX_TOKEN')
 api = ZabbixAPI(url="https://zabbix.ageri.com.br/")
 api.login(token={ZABBIX_PASSWORD})
 
-git_archive_name = subprocess.run(["git", "diff", "--name-only", "origin/master", "--", "templates/"], capture_output=True, text=True)
-file_changed = git_archive_name.stdout.strip().splitlines()
-print(file_changed)
+git_archive_name = subprocess.run(["git", "diff", "--name-only", "HEAD@{1}", "HEAD", "--relative", "templates/"], capture_output=True, text=True)
+changed_files = [file.strip() for file in result.stdout.splitlines() if file.strip()]
 
+print("Arquivos modificados no último push:")
+print("\n".join(changed_files))
 
-yaml_content = ''
-with open(f"./{file_changed}", "r", encoding="utf-8") as f:
-     yaml_content = f.read()
+for i in changed_files:
+    yaml_content = ''
+    with open(f"./{i}", "r", encoding="utf-8") as f:
+        yaml_content = f.read()
 
-print(yaml_content)
-# Requisição de importação
-result = api.configuration.import_({
-    "source": yaml_content,
-    "format": "yaml",
-    "rules": {
-        "templates": {
-            "createMissing": True,
-            "updateExisting": True
-        },
-        "items": {
-            "createMissing": True,
-            "updateExisting": True,
-            "deleteMissing": True
-        },
-        "triggers": {
-            "createMissing": True,
-            "updateExisting": True,
-            "deleteMissing": True
-        },
-        "valueMaps": {
-            "createMissing": True,
-            "updateExisting": False
-        }
-    }})
+    # Requisição de importação
+    result = api.configuration.import_({
+        "source": yaml_content,
+        "format": "yaml",
+        "rules": {
+            "templates": {
+                "createMissing": True,
+                "updateExisting": True
+            },
+            "items": {
+                "createMissing": True,
+                "updateExisting": True,
+                "deleteMissing": True
+            },
+            "triggers": {
+                "createMissing": True,
+                "updateExisting": True,
+                "deleteMissing": True
+            },
+            "valueMaps": {
+                "createMissing": True,
+                "updateExisting": False
+            }
+        }})
 
-print("Importação concluída:", result)
+    print("Importação concluída:", result)
 
