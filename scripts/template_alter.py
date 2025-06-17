700a2f29cd45ee206993f0beb0ace0807e70f623
DennisNgrox <dennis.silva@ageri.com.br>
2025-06-17 16:43:51 -0300

    add

--- scripts/template_alter.py
+++ scripts/template_alter.py
@@ -9,14 +9,12 @@ api.login(token={ZABBIX_PASSWORD})
 git_archive_name = subprocess.run(["git", "diff", "--name-only", "HEAD@{1}", "HEAD", "--relative", "templates/"], capture_output=True, text=True)
 changed_files = [file.strip() for file in git_archive_name.stdout.splitlines() if file.strip()]
 
-print("Arquivos modificados no último push:")
-print("\n".join(changed_files))
 
 for i in changed_files:
     yaml_content = ''
     with open(f"./{i}", "r", encoding="utf-8") as f:
         yaml_content = f.read()
-
+ 
     # Requisição de importação
     result = api.configuration.import_({
         "source": yaml_content,
